7a3672ea99f36a23f8d92eee3d622ec1249b4b61
Kozinak <yroslavbot1@gmail.com>
2026-03-11 09:23:05 +0300

    Двухфакторная аутентификация

--- Test1/landing/views.py
+++ Test1/landing/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def landing(request):
-    return render(request, 'landing/index.html')
+    return HttpResponse('<h1>Ответ получен</h1>')
